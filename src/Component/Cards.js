8b5adfd5d7c44500f481225c986a354965df7897
kedar-devs <67545340+kedar-devs@users.noreply.github.com>
2021-07-02 14:42:47 +0530

    Fix:try to sort the cors error

--- src/Component/Cards.js
+++ src/Component/Cards.js
@@ -1,30 +1,37 @@
 import React, { useState } from 'react'
-import QRCode  from 'qrcode.react'
+import QRCode from 'qrcode.react'
 
 import { Document, Page } from 'react-pdf'
 import { pdfjs } from 'react-pdf';
 function Cards(props) {
   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
-    const [pageNumber,setPage]=useState(1)
-    const  onDocumentLoad = () => {
-      setPage(1)
-    }
-    return (
-        <div className='border-4'>
-                {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
-                {props.data.type==='image'?<img src={props.data.thumbnail} alt='sorry server error'/>:<Document
-                file={props.data.link}
-                onLoadSuccess={onDocumentLoad}>
-                    <Page pageNumber={pageNumber} />
-                  </Document>
+  const [pageNumber, setPage] = useState(1)
+  const onDocumentLoad = () => {
+    setPage(1)
+  }
+  return (
+    <div className='border-4'>
+      {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+      {props.data.type === 'image' ? <img src={props.data.thumbnail} alt='sorry server error' /> : <Document
+        file={{
+          url: `${props.data.link}`, httpHeaders: {
+            'Access-Control-Request-Method': 'GET',
+            "Access- Control-Allow-Origin": "*"
+          }, withCredentials: true
+        }}
+        onLoadSuccess={onDocumentLoad}
+        renderMode="canvas"
+      >
+        <Page pageNumber={pageNumber} />
+      </Document>
 
-        } 
-                <QRCode value={props.data.link} src="holder.js/100px180" />
-               
-                <h1>{props.data.name}</h1>
-     
-        </div>
-    )
+      }
+      <QRCode value={props.data.link} src="holder.js/100px180" />
+
+      <h1>{props.data.name}</h1>
+
+    </div>
+  )
 }
 
 export default Cards
